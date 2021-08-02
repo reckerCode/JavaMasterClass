4b38063dd636300ae554b4f4c87a1fe2dade7e93
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-02 15:59:06 -0400

    .

---
+++ doWhile/src/com/reckercode/Main.java
@@ -0,0 +1,24 @@
+package com.reckercode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count =1;
+        while(count!=6){
+            System.out.println("Count value is: "+ count);
+            count++;
+        }
+
+        count =1;
+        while(true){
+            if(count==6){
+
+                break;
+            }
+            System.out.println("Count value is: "+count);
+            count++;
+        }
+
+    }
+}
