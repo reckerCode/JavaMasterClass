c87b187325187655f4ff88babf47ff6cc17b600f
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-03 15:11:53 -0400

    First and Last Digit sum

---
+++ FirstAndLastDigitSum/src/com/reckercode/Main.java
@@ -0,0 +1,27 @@
+package com.reckercode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number = 123;
+        System.out.println("First and last digit sum: "+sumFirstAndLastDigit(number));
+    }
+    public static int sumFirstAndLastDigit(int number){
+
+        if(number<0){
+            return 0;
+        }
+        int sum = number%10;
+        while(number>0){
+            int digit = number%10;
+            int temp = number;
+            number/=10;
+            if(number==0){
+                sum+=temp;
+
+            }
+        }
+        return sum;
+    }
+}
