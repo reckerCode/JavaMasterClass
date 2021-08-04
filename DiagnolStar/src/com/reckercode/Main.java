2a60487cba92d98b9fa152b26c88fc18d0a7348d
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-03 21:31:43 -0400

    Diagonal Star, github version - Tim Buchalka

---
+++ DiagnolStar/src/com/reckercode/Main.java
@@ -0,0 +1,30 @@
+package com.reckercode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquareStar(8);
+        System.out.println();
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid number");
+            return;
+        }
+        for (int row = 1; row <= number; row++) {
+            for (int col = 1; col <= number; col++) {
+                if (row == 1 || row == number || col == 1 || col == number) {
+                    System.out.print("*");
+                } else if (row == col) {
+                    System.out.print("*");
+                } else if (col == (number - row + 1)) {
+                    System.out.print("*");
+                } else
+                    System.out.print(" ");
+            }// close inner loop
+            System.out.println();
+
+        }// close outter loop
+    }
+}
---
+++ DiagonalStar/src/com/reckercode/Main.java
@@ -0,0 +1,15 @@
+package com.reckercode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+    public static void printSquareStar(int number){
+        if(number<5){
+            System.out.println("Invalid Value");
+        }
+        
+    }
+}
