91b8e8537704d4e86e469b9e83c06108fb7eabef
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-05 09:25:21 -0400

    Bank accounts with constructor

---
+++ BankAccount/src/com/reckercode/BankAccount.java
@@ -0,0 +1,75 @@
+package com.reckercode;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private int phoneNumber;
+
+    public BankAccount(){
+        System.out.println("Empty Constructor");
+    }
+
+    public BankAccount(String accountNumber, double balance, String customerName, String email, int phoneNumber){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public void deposit(double amount){
+        this.balance=this.balance+amount;
+        System.out.println("Balance is "+this.balance);
+    }
+    public void withdraw(double amount){
+        if(amount>balance){
+            System.out.println("Insufficient Balance");
+        }else {
+            this.balance = this.balance - amount;
+            System.out.println("Balance is "+this.balance);
+        }
+    }
+
+    public String  getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
