1497dd2ec1248b24b2d147e91bd19c44398b3d67
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-06 14:28:54 -0400

    composition

---
+++ InheritanceChallenge/src/com/reckercode/PC.java
@@ -0,0 +1,24 @@
+package com.reckercode;
+
+public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private MotherBoard motherboard;
+
+    public PC(Case theCase, Monitor monitor, MotherBoard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp(){
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo(){
+
+        monitor.drawPixelAt(1200,500, "yello");
+
+    }
+}
