ba670b052bfb0ffe9e1d3ff063a7e8d2f5a9ddb1
HardikGunjal <46985637+reckerhardik@users.noreply.github.com>
2021-08-12 20:46:01 -0400

    Google Foobar Challenge Solutions.

---
+++ FoobarChallenge/src/com/reckercode/Main.java
@@ -0,0 +1,10 @@
+package com.reckercode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+        System.out.println(Solution.solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"));
+        System.out.println(Solution.solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?"));
+    }
+}
